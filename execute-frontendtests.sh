d0789f6fdb5dcb1c58b6b7b17327e1dda2589e64
iain.mackay <iain.mackay4@nhs.net>
2022-07-27 15:13:20 +0000

    Execute tests from script file

---
+++ execute-frontendtests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Execute the front-end tests for this repo
+
+# Test are executed in a temporary subdirectory
+# Report files from the tests are available there
+
+# Requires these environment values:
+
+# BASE_URL: URL of the deployment to be tested (https:// + domain)
+# SECRETS_FILE: Path to CSV file containing username/password pair(s)
+# REPO_USERNAME: Username for the Docker repository with the Front End test image
+# REPO_PASSWORD: Password "
+
+# Can't have empty value for TAGS as pipeline parameter so change "all" to "" here
+TAGS=$([ "$TAGS" = "all" ] && echo "" || echo "$TAGS")
+echo "Effective TAGS: '$TAGS'"
+
+WORK=$(mktemp -d -t frontendtest-XXXXXXXXXX)
+echo "Test ${BASE_URL:?No deployment URL specified (BASE_URL)} in $WORK with tags: $TAGS"
+
+cp FrontEndTests $WORK
+mkdir $WORK/work
+cd $WORK/work
+
+echo "### Running docker image"
+docker login dctimages.azurecr.io -u ${REPO_USERNAME:?No username for the Docker repo (REPO_PASSWORD)} -p ${REPO_PASSWORD:?No password for the Docker repo (REPO_PASSWORD)}
+docker pull dctimages.azurecr.io/acceptancetests
+docker run \
+  --env BASE_URL \
+  --env TAGS \
+  --mount type=bind,source=$WORK/FrontEndTests,target=/automation-ui/FrontEndTests \
+  --mount type=bind,source=${SECRETS_FILE:?No secrets file specified (SECRETS_FILE)},target=/automation-ui/login.csv \
+  dctimages.azurecr.io/acceptancetests
+echo "##vso[task.setvariable variable=PASSED;]$?"
+echo "Status of tests: $PASSED"
+exit $PASSED
