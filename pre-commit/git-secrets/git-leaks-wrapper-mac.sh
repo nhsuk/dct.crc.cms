af6d52544d619c1730b271acd4655b8ee466a815
iain.mackay <iain.mackay4@nhs.net>
2022-08-03 14:10:41 +0000

    Fix page-picker search bug

--- pre-commit/git-secrets/git-leaks-wrapper-mac.sh
+++ pre-commit/git-secrets/git-leaks-wrapper-mac.sh
@@ -3,4 +3,4 @@
 # Note that this will be invoked by the git hook from the repo root, so cd .. isn't required
 
 # Scan the current commit
-gitleaks protect --config "./git-secrets/nhsd-gitleaks-config.toml" --verbose --staged
+gitleaks protect --config "./pre-commit/git-secrets/nhsd-gitleaks-config.toml" --verbose --staged
