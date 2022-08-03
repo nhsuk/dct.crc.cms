7d1cdb9e644a6de5ee5da529be963f710cfe547b
Sikander Ali <sikander.ali@nhs.net>
2022-08-03 12:17:37 +0100

    simplify gitleaks

--- pre-commit/git-secrets/git-leaks-wrapper-mac.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Note that this will be invoked by the git hook from the repo root, so cd .. isn't required
-
-# Scan the current commit
-gitleaks protect --config "./pre-commit/git-secrets/nhsd-gitleaks-config.toml" --verbose --staged
