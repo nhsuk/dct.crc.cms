257fe93d7f2c9c770b16db1e523ff9e171f518d2
Sikander Akbar Ali <sikander.ali@nhs.net>
2022-08-09 10:59:10 +0100

    simplify gitleaks (#83)
    * simplify gitleaks
    
    * add config file to gitleaks
    
    * fix pr-test toml location
    
    * fix broken pipeline
    
    Co-authored-by: Ross Clark <ross_clark@outlook.com>

--- pre-commit/git-secrets/git-leaks-wrapper.bat
+++
@@ -1,5 +0,0 @@
-:: Calls gitleaks on the current repo using local config
-gitleaks --repo-config-path="./pre-commit/git-secrets/nhsd-gitleaks-config.toml" --verbose
-
-:: Scan all files within this repo for this commit
-:: gitleaks --path=. --repo-config-path="./git-secrets/nhsd-gitleaks-config.toml" --verbose
