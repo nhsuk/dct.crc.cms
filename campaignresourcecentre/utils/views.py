937bb8ea25dda5c2f65bbfa76dcc7c4bcfc49542
iain.mackay <iain.mackay4@nhs.net>
2022-08-10 13:56:56 +0000

    Consolidated changes for CV-712/CV-721

--- campaignresourcecentre/utils/views.py
+++ campaignresourcecentre/utils/views.py
@@ -1,6 +1,10 @@
 from django.views import defaults
 
 
+def bad_request(request, template_name="errors/400.html"):
+    return defaults.bad_request(request, template_name)
+
+
 def page_not_found(request, exception, template_name="errors/404.html"):
     return defaults.page_not_found(request, exception, template_name)
 
