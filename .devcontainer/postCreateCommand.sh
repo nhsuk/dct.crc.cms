90248a512a86da40d254e6ad210f68d3d4af93aa
iain.mackay <iain.mackay4@nhs.net>
2022-09-06 15:49:35 +0000

    Add pre-commit hooks in dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+# Install go
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+rm go1.19.linux-amd64.tar.gz
+echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
+# Install gitleaks
+wget https://github.com/zricethezav/gitleaks/releases/download/v8.12.0/gitleaks_8.12.0_linux_x64.tar.gz
+sudo tar -C /usr/local/bin -xzf gitleaks_8.12.0_linux_x64.tar.gz
+rm gitleaks_8.12.0_linux_x64.tar.gz
+echo 'export PATH="$PATH:/opt/poetry/bin"' >> ~/.bashrc
 az config set extension.use_dynamic_install=yes_without_prompt
+pre-commit install
