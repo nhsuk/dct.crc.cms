a0da4a0e75d02e53564324fb34465f002fb54de7
iain.mackay <iain.mackay4@nhs.net>
2022-09-16 16:30:55 +0000

    install psql to access local database

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Install go
+
+# Install go for gitleaks
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 rm go1.19.linux-amd64.tar.gz
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
+
 # Install gitleaks
 wget https://github.com/zricethezav/gitleaks/releases/download/v8.12.0/gitleaks_8.12.0_linux_x64.tar.gz
 sudo tar -C /usr/local/bin -xzf gitleaks_8.12.0_linux_x64.tar.gz
@@ -11,3 +13,11 @@ rm gitleaks_8.12.0_linux_x64.tar.gz
 echo 'export PATH="$PATH:/opt/poetry/bin"' >> ~/.bashrc
 az config set extension.use_dynamic_install=yes_without_prompt
 pre-commit install
+
+# Install psql
+sudo apt-get install -y postgresql-client
+echo 'export PGUSER=campaignresourcecentre' >> ~/.bashrc
+echo 'export PGPASSWORD=campaignresourcecentre' >> ~/.bashrc
+echo 'export PGDATABASE=campaignresourcecentre' >> ~/.bashrc
+echo 'export PGUSER=campaignresourcecentre' >> ~/.bashrc
+echo 'export PGHOSTADDR=0.0.0.0' >> ~/.bashrc
