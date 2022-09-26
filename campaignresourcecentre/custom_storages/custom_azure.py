b4cc933cfa3bde8710878cbb342b54dbad9271a1
Ross <43380330+Ross-Clark@users.noreply.github.com>
2022-09-26 22:15:55 +0100

    Add additional storage backend for search (#117)
    The private backend implements SAS Tokens for accessing wagtail documents stored in azure blob containers in the same storage account as the rest of the media for crc

--- campaignresourcecentre/custom_storages/custom_azure.py
+++ campaignresourcecentre/custom_storages/custom_azure.py
@@ -37,6 +37,32 @@ class AzureMediaStorage(AzureStorage):
         return new_url
 
 
+class AzureDocStorage(AzureStorage):
+    account_name = getattr(settings, "AZURE_ACCOUNT_NAME", None)
+    account_key = getattr(settings, "AZURE_ACCOUNT_KEY", None)
+    azure_container = getattr(settings, "AZURE_DOC_CONTAINER", None)
+    external_domain = getattr(settings, "AZURE_CUSTOM_DOMAIN", None)
+    custom_domain = None
+
+    def save(self, name, content, max_length=None):
+        logger.info(
+            "Saving %s in container %s of account %s"
+            % (name, __class__.azure_container, __class__.account_name)
+        )
+        return super().save(name, content, max_length)
+
+    def __init__(self, **settings):
+        super().__init__(**settings)
+        logger.info("Using storage domain '%s' for Docs", self.custom_domain)
+
+    def url(self, name, expire=7200, parameters=None):
+        initial_url = super().url(name, expire, parameters)
+        components = list(urlparse(initial_url))
+        components[1] = self.external_domain
+        new_url = urlunparse(components)
+        return new_url
+
+
 class AzureSearchStorage(AzureStorage):
     account_name = getattr(settings, "AZURE_SEARCH_STORAGE_ACCOUNT_NAME", None)
     account_key = getattr(settings, "AZURE_SEARCH_ACCESS_KEY", None)
@@ -60,3 +86,13 @@ class SearchStorage(LazyObject):
 
 
 search_storage = SearchStorage()
+
+
+class DocStorage(LazyObject):
+    def _setup(self):
+        self._wrapped = get_storage_class(
+            getattr(settings, "DOC_STORAGE_CLASS", settings.DEFAULT_FILE_STORAGE)
+        )()
+
+
+doc_storage = DocStorage()
--- campaignresourcecentre/documents/models.py
+++ campaignresourcecentre/documents/models.py
@@ -1,6 +1,14 @@
 from wagtail.documents.models import AbstractDocument
 from wagtail.documents.models import Document as WagtailDocument
 
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from campaignresourcecentre.custom_storages.custom_azure import doc_storage
+
 
 class CustomDocument(AbstractDocument):
     admin_form_fields = WagtailDocument.admin_form_fields
+    file = models.FileField(
+        upload_to="documents", storage=doc_storage, verbose_name=_("file")
+    )
