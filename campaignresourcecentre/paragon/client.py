cab325a4f5bf62707f2c81e3912c19b93b50ae9d
Cornelius Jideofor <jideofor.cornelius@gmail.com>
2022-10-11 08:05:03 +0100

    registration reporting test

--- campaignresourcecentre/paragon/client.py
+++ campaignresourcecentre/paragon/client.py
@@ -140,9 +140,7 @@ class Client:
             #     "profileId": "",
             # })
 
-            logger.info(
-                f"-------------------- {self.response.content} -------------------"
-            )
+            logger.info(f"-------------------- {self.response} -------------------")
             # data_dump = self.response.content
             # send_report("registration", data_dump)
 
