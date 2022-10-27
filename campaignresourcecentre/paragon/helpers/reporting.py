33da09d2eba9ef7e13b696648f87eb75d3766c44
Cornelius Jideofor <jideofor.cornelius@gmail.com>
2022-10-27 12:35:03 +0100

    rebase to master

--- campaignresourcecentre/paragon/helpers/reporting.py
+++ campaignresourcecentre/paragon/helpers/reporting.py
@@ -7,20 +7,8 @@ logger = logging.getLogger(__name__)
 
 def send_report(event_type, params={}):
 
-<<<<<<< HEAD
     url = settings.REPORTING_ENDPOINT
     payload = json.dumps({"payload": params, "event": event_type, "group": "crc"})
-=======
-    # queue = "queue={}".format(settings.REPORTING_QUEUE)
-    # message = ""
-    # query_string = "queue={}&message={}".format(queue, message)
-    # url = "{}?{}".format(settings.REPORTING_ENDPOINT, query_string)
-
-    url = settings.REPORTING_ENDPOINT
-    payload = json.dumps(
-        {"queue": settings.REPORTING_QUEUE, "message": params, "event": event_type}
-    )
->>>>>>> registration reporting test
 
     headers = {
         "Content-Type": "application/json; charset=UTF-8",
@@ -31,7 +19,6 @@ def send_report(event_type, params={}):
             response = requests.post(url, headers=headers, data=payload)
             if response.ok:
                 logger.info(
-<<<<<<< HEAD
                     "Successfully sent reporting data for user {} : {}".format(
                         event_type, response.content
                     )
@@ -41,13 +28,6 @@ def send_report(event_type, params={}):
                     "Error sending reporting data for user {} : {}".format(
                         event_type, response.content
                     )
-=======
-                    "Successfully sent {} report: {}".format(event_type, response)
-                )
-            else:
-                logger.info(
-                    "Error sending {} report: {}".format(event_type, response.content)
->>>>>>> registration reporting test
                 )
         except Exception as err:
             logger.error("Exception raised : %s", err)
