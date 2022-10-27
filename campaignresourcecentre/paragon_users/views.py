90b379a985690610570c02bbce69160eb1a031a9
Cornelius Jideofor <jideofor.cornelius@gmail.com>
2022-10-27 09:06:34 +0100

    created entry point to capture crc event from front end and send data via api

--- campaignresourcecentre/paragon_users/views.py
+++ campaignresourcecentre/paragon_users/views.py
@@ -127,40 +127,6 @@ def signup(request):
                     )
                     send_report("registration", data_dump)
 
-                    # send off data to reporting
-                    data_dump = json.dumps(
-                        {
-                            "userToken": token,
-                            "createdAt": created_at,
-                            "organisation": organisation,
-                            "jobTitle": job_title,
-                            "role": get_role(email, job_title),
-                            "postcode": postcode,
-                            "longitude": postcode_data.get("longitude"),
-                            "latitude": postcode_data.get("latitude"),
-                            "region": postcode_data.get("region"),
-                            "date": timezone.now().strftime("%Y-%m-%d"),
-                        }
-                    )
-                    send_report("registration", data_dump)
-
-                    # send off data to reporting
-                    data_dump = json.dumps(
-                        {
-                            "userToken": token,
-                            "createdAt": created_at,
-                            "organisation": organisation,
-                            "jobTitle": job_title,
-                            "role": get_role(email, job_title),
-                            "postcode": postcode,
-                            "longitude": get_postcode_data(postcode).get("longitude"),
-                            "latitude": get_postcode_data(postcode).get("latitude"),
-                            "region": get_postcode_data(postcode).get("region"),
-                            "date": timezone.now().strftime("%Y-%m-%d"),
-                        }
-                    )
-                    send_report("registration", data_dump)
-
                     return render(
                         request,
                         "users/confirmation_registration.html",
