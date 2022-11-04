aacf265455cf0b4552dc0ac22bbd185a9c66679d
Ross <43380330+Ross-Clark@users.noreply.github.com>
2022-11-04 09:39:38 +0000

    add coverage to sonarqube

---
+++ docker/coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo -u campaignresourcecentre
+python manage.py migrate
+echo "1"
+python manage.py test --noinput 
+echo "2"
+coverage erase 
+echo "3"
+coverage run manage.py test --settings=campaignresourcecentre.settings.test
+echo "4"
+coverage xml -i 
+echo "5"
+mv ./coverage.xml ./docker
