572e7a6fa944391c7fc039261839f03a2222d378
Ross Clark <ross_clark@outlook.com>
2022-12-01 15:45:00 +0000

    only enforce 2fa on prod settings

--- campaignresourcecentre/settings/production.py
+++ campaignresourcecentre/settings/production.py
@@ -11,3 +11,6 @@ SESSION_COOKIE_SECURE = True
 # Ensure that the CSRF cookie is only sent by browsers under an HTTPS connection.
 # https://docs.djangoproject.com/en/stable/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = True
+
+# forces users to use 2fa flow on staging and prod
+WAGTAIL_2FA_REQUIRED = True
