5d3ce620dc7554e03d45aca8a38b0d321675bc55
Iain MacKay <iain-mackay4@nhs.net>
2023-01-06 15:03:06 +0000

    Correct get_parent tag implementation

--- campaignresourcecentre/core/templatetags/page_tags.py
+++ campaignresourcecentre/core/templatetags/page_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_parent(context):
     page = context.get("page", None)
-    parent = page.get_parent(update=True)
+    parent = page.get_parent(update=True) if page else None
     return parent or "Campaign Resource Centre"
