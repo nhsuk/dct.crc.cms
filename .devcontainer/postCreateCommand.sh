5890e638871f52edcd24afd448ac3b15197856b2
Iain MacKay <iain-mackay4@nhs.net>
2023-01-30 17:29:13 +0000

    Refactor devcontainer and upgrade poetry

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# Install go
-wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-rm go1.19.linux-amd64.tar.gz
-echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
-# Install gitleaks
-wget https://github.com/zricethezav/gitleaks/releases/download/v8.12.0/gitleaks_8.12.0_linux_x64.tar.gz
-sudo tar -C /usr/local/bin -xzf gitleaks_8.12.0_linux_x64.tar.gz
-rm gitleaks_8.12.0_linux_x64.tar.gz
-echo 'export PATH="$PATH:/opt/poetry/bin"' >> ~/.bashrc
+
+# It's a nuisance to have to confirm this stuff
 az config set extension.use_dynamic_install=yes_without_prompt
+
+# Install requirements for operating the devcontainer (application requirements installed in application Dockerfile)
+
+pip install -r ./.devcontainer/devrequirements.txt --user
+
+# Install pre-commit hooks
 pre-commit install
