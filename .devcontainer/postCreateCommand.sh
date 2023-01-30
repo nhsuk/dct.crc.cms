200623598782bc0cd8e9e49b737cd38926b98dcb
Iain MacKay <iain-mackay4@nhs.net>
2023-01-30 17:45:51 +0000

    Flag workspaces folder as git safe

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# This comes up a lot with mounting stuff into stuff
+git config --global --add safe.directory /workspaces/dct-campaign-resource-centre
+
 # It's a nuisance to have to confirm this stuff
 az config set extension.use_dynamic_install=yes_without_prompt
 
 # Install requirements for operating the devcontainer (application requirements installed in application Dockerfile)
-
 pip install -r ./.devcontainer/devrequirements.txt --user
 
 # Install pre-commit hooks
