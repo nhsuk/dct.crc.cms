2792bd2747b2a6832352098dfdb2fa266e99f78f
Iain MacKay <imm@iain-mackay.org>
2023-02-06 12:48:32 +0000

    Support production build in local build

--- docker/bashrc.sh
+++ docker/bashrc.sh
@@ -2,9 +2,7 @@
 # Note: gunicorn requires at least two workers to avoid request blocking
 
 alias dj="python manage.py"
+alias djrun="python manage.py runserver 0.0.0.0:8000"
+alias djgun="gunicorn campaignresourcecentre.wsgi:application --reload"
+alias djtest="python manage.py test --settings=campaignresourcecentre.settings.test"
 
-if [ "$BUILD_ENV" = "dev" ]; then
-    alias djrun="python manage.py runserver 0.0.0.0:8000"
-    alias djgun="gunicorn campaignresourcecentre.wsgi:application --reload"
-    alias djtest="python manage.py test --settings=campaignresourcecentre.settings.test"
-fi
--- manage.py
+++ manage.py
@@ -11,10 +11,13 @@ if __name__ == "__main__":
 
     if settings.DEBUG:
         if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
-            import ptvsd
+            try:
+                import ptvsd
 
-            ptvsd.enable_attach(address=("0.0.0.0", 4000))
-            print("Debugger attached on port 4000!")
+                ptvsd.enable_attach(address=("0.0.0.0", 4000))
+                print("Debugger attached on port 4000!")
+            except ImportError:
+                print("Not a dev build, can't attach debugger")
 
     from django.core.management import execute_from_command_line
 
