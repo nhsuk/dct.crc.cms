40c84aa4389bcbccb0d73ce94b37d9f1bdba51d6
Ross <43380330+Ross-Clark@users.noreply.github.com>
2023-03-01 10:48:12 +0000

    Review/schedule (#237)
    * add hook for publishing pages
    
    * add tests to scheduled tasks and improve logging
    
    * make sonarqube happy
    
    * make linting happy

---
+++ campaignresourcecentre/scheduled_tasks/views.py
@@ -0,0 +1,33 @@
+import logging
+from django.core.management import call_command
+from django.core.exceptions import PermissionDenied
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.utils import timezone
+
+from wagtail.core.models import Page
+
+logger = logging.getLogger(__name__)
+
+
+@require_http_methods(["GET"])
+def publish_pages(request):
+    """Publish pages that are scheduled to be published"""
+
+    pubtoken = getattr(settings, "PUBTOKEN", None)
+    try:
+        if request.headers.get("Authorization", "") == "Bearer " + pubtoken:
+            future_pages = len(Page.objects.filter(go_live_at__gt=timezone.now()))
+            if future_pages > 0:
+                logger.info("publishing" + future_pages + "scheduled pages")
+                call_command("publish_scheduled_pages")
+            else:
+                logger.info("No scehduled pages")
+            return HttpResponse("ok", status=202)
+    except TypeError:
+        logger.warning("PUBTOKEN not set")
+        raise PermissionDenied
+    # return 401 django error page
+    logger.warning("Unauthorized publish_pages request")
+    raise PermissionDenied
--- campaignresourcecentre/settings/test.py
+++ campaignresourcecentre/settings/test.py
@@ -19,3 +19,4 @@ SECURE_SSL_REDIRECT = False
 # By default, Django uses a computationally difficult algorithm for passwords hashing.
 # We don't need such a strong algorithm in tests, so use MD5
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+PUBTOKEN = "correct"
