19c60e15cc394add392554297ed0a6b9e7d55a1c
Iain MacKay <iain.mackay4@nhs.net>
2023-04-12 17:08:41 +0000

    Add search orphan management

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,9 +10,11 @@ rm go1.19.linux-amd64.tar.gz
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
 
 # Install gitleaks
-wget https://github.com/zricethezav/gitleaks/releases/download/v8.12.0/gitleaks_8.12.0_linux_x64.tar.gz
-sudo tar -C /usr/local/bin -xzf gitleaks_8.12.0_linux_x64.tar.gz
-rm gitleaks_8.12.0_linux_x64.tar.gz
+wget https://github.com/gitleaks/gitleaks/releases/download/v8.16.2/gitleaks_8.16.2_linux_x64.tar.gz
+sudo tar -C /usr/local/bin -xzf gitleaks_8.16.2_linux_x64.tar.gz
+rm gitleaks_8.16.2_linux_x64.tar.gz
+
+# Configure the environment
 echo 'export PATH="$PATH:/opt/poetry/bin"' >> ~/.bashrc
 az config set extension.use_dynamic_install=yes_without_prompt
 pre-commit install
