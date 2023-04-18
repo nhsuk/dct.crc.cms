2fa34f040d894efc227db0610e5b48b4bf53d56a
Sue Johnson <suesdesign@users.noreply.github.com>
2023-04-18 17:17:30 +0100

    Review/cv 1074 2 (#247)
    * Anti spam form field
    
    * Form unit test

--- campaignresourcecentre/standardpages/views.py
+++ campaignresourcecentre/standardpages/views.py
@@ -1,6 +1,8 @@
 from io import StringIO
 import sys
 
+from logging import getLogger
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.conf import settings
@@ -18,6 +20,8 @@ from campaignresourcecentre.notifications.dataclasses import ContactUsData
 from campaignresourcecentre.notifications.adapters import gov_notify_factory
 
 from .forms import ContactUsForm
+
+logger = getLogger(__name__)
 
 
 @require_http_methods(["GET"])
@@ -38,7 +42,7 @@ def cookie_confirmation(request):
 @require_http_methods(["POST", "GET"])
 def contact_us(request):
     contact_us_form = ContactUsForm()
-    if request.POST:
+    if request.method == "POST":
         contact_us_form = ContactUsForm(request.POST)
         campaign = contact_us_form.data.get("healthy_behaviour")
         if campaign == "other":
@@ -49,28 +53,32 @@ def contact_us(request):
         if job_title == "student":
             contact_us_form.fields.get("organisation").required = False
 
-        if contact_us_form.is_valid():
-            payload = contact_us_form.data
-            contact_us_data = ContactUsData(
-                payload.get("email"),
-                payload.get("first_name"),
-                payload.get("last_name"),
-                payload.get("job_title"),
-                payload.get("organisation"),
-                payload.get("organisation_type"),
-                campaign,
-                payload.get("audience"),
-                payload.get("engage_audience"),
-                payload.get("product_service"),
-                payload.get("message"),
-            )
+        # Check the hidden form field has not been filled in by a spammer.
+        if contact_us_form.data.get("cat6a"):
+            logger.info("Spam detection field completed.")
+        else:
+            if contact_us_form.is_valid():
+                payload = contact_us_form.data
+                contact_us_data = ContactUsData(
+                    payload.get("email"),
+                    payload.get("first_name"),
+                    payload.get("last_name"),
+                    payload.get("job_title"),
+                    payload.get("organisation"),
+                    payload.get("organisation_type"),
+                    campaign,
+                    payload.get("audience"),
+                    payload.get("engage_audience"),
+                    payload.get("product_service"),
+                    payload.get("message"),
+                )
 
-            gov_notify_factory().contact_submission(
-                settings.PHE_PARTNERSHIPS_EMAIL,
-                f"[CRC Contact Request] {campaign}",
-                contact_us_data,
-            )
-            return render(request, "thank_you_page.html")
+                gov_notify_factory().contact_submission(
+                    settings.PHE_PARTNERSHIPS_EMAIL,
+                    f"[CRC Contact Request] {campaign}",
+                    contact_us_data,
+                )
+                return render(request, "thank_you_page.html")
     return render(request, "contact-us.html", {"form": contact_us_form})
 
 
