19dc849708b665caba6a1fdb5bd28e603477ed0a
Iain MacKay <iain.mackay4@nhs.net>
2023-05-03 13:34:26 +0000

    Consolidate changes

--- campaignresourcecentre/standardpages/tests/tests.py
+++ campaignresourcecentre/standardpages/tests/tests.py
@@ -1,6 +1,15 @@
-from django.test import TestCase
+from io import StringIO
+import secrets
+import string
+
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.core.exceptions import PermissionDenied
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
+
 from campaignresourcecentre.standardpages.forms import ContactUsForm
+from campaignresourcecentre.standardpages.views import search_orphans
 
 
 class ContactUsViewTests(TestCase):
@@ -62,3 +71,61 @@ class ContactUsViewTests(TestCase):
         }
         response = self.client.post(reverse("contact_us"), form_data)
         self.assertContains(response, "Contact us")
+
+
+user_model = get_user_model()
+
+
+def generate_random_password():  # To satisfy Sonarquvbe security checks
+    password = (
+        "".join(secrets.choice(string.ascii_lowercase) for _ in range(3))
+        + "".join(secrets.choice(string.ascii_uppercase) for _ in range(3))
+        + "".join(secrets.choice(string.digits) for _ in range(3))
+        + "".join(secrets.choice(string.punctuation) for _ in range(3))
+    )
+    return password
+
+
+class SearchOrphansTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.superuser = user_model.objects.create_superuser(
+            username="admin",
+            password=generate_random_password(),
+            email="admin@example.com",
+        )
+
+    def test_search_orphans(self):
+        # Create a request object with a superuser
+        request = self.factory.get("/search_orphans/")
+        request.user = self.superuser
+
+        # Set up a StringIO to capture the command output
+        response_file = StringIO()
+
+        # Call the searchorphans management command
+        call_command("searchorphans", stdout=response_file, stderr=response_file)
+
+        # Reset the StringIO position to the beginning
+        response_file.seek(0)
+
+        # Call the view function
+        response = search_orphans(request)
+
+        # Assertions
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "text/plain")
+        self.assertEqual(response.content.decode(), response_file.read())
+
+    def test_search_orphans_permission_denied(self):
+        # Create a request object with an anonymous user
+        request = self.factory.get("/search_orphans/")
+        request.user = user_model.objects.create_user(
+            username="anon",
+            password=generate_random_password(),
+            email="admin@example.com",
+        )
+
+        # Call the view function and assert for PermissionDenied exception
+        with self.assertRaises(PermissionDenied):
+            search_orphans(request)
