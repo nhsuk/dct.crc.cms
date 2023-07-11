f46575fd5ac0e9ecfeae3744235818fad7ca04a5
Sikander Akbar Ali <sikander.ali@nhs.net>
2023-07-11 16:50:39 +0100

    fix displaying of taxonomies

--- campaignresourcecentre/core/wagtail_hooks.py
+++ campaignresourcecentre/core/wagtail_hooks.py
@@ -5,3 +5,14 @@ from django.http import HttpResponseBadRequest
 from wagtail import hooks
 
 logger = getLogger(__name__)
+
+from django.utils.html import format_html
+from django.templatetags.static import static
+
+
+@hooks.register("insert_global_admin_css")
+def insert_global_admin_css():
+    return format_html(
+        '<link rel="stylesheet" type="text/css" href="{}">',
+        static("admin.css"),
+    )
