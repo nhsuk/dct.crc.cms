a928c74d852720d55dacce244ede0b920c370ecf
Ross <43380330+Ross-Clark@users.noreply.github.com>
2023-09-20 11:05:00 +0100

    Review/add doc storage (#284)
    * add doc storage
    
    * fix error when there is no doc container

--- campaignresourcecentre/custom_storages/custom_azure.py
+++ campaignresourcecentre/custom_storages/custom_azure.py
@@ -78,6 +78,10 @@ class AzureMediaStorage(AzureStorage):
         return AzureMediaStorageFile(name, mode, self)
 
 
+class AzureDocStorage(AzureMediaStorage):
+    azure_container = getattr(settings, "AZURE_DOC_CONTAINER", None)
+
+
 # Class to present a Python file interface to an Azure blob.
 # Surprisingly, no prior implementation seems to exist
 
@@ -311,6 +315,32 @@ class AzureUploadedFile(UploadedFile):
         super().__init__(file, name, content_type, size, charset, content_type_extra)
 
 
+class AzureDocStorage(AzureStorage):
+    account_name = getattr(settings, "AZURE_ACCOUNT_NAME", None)
+    account_key = getattr(settings, "AZURE_ACCOUNT_KEY", None)
+    azure_container = getattr(settings, "AZURE_DOC_CONTAINER", None)
+    external_domain = getattr(settings, "AZURE_CUSTOM_DOMAIN", None)
+    custom_domain = None
+
+    def save(self, name, content, max_length=None):
+        logger.info(
+            "Saving %s in container %s of account %s"
+            % (name, __class__.azure_container, __class__.account_name)
+        )
+        return super().save(name, content, max_length)
+
+    def __init__(self, **settings):
+        super().__init__(**settings)
+        logger.info("Using storage domain '%s' for Docs", self.custom_domain)
+
+    def url(self, name, expire=7200, parameters=None):
+        initial_url = super().url(name, expire, parameters)
+        components = list(urlparse(initial_url))
+        components[1] = self.external_domain
+        new_url = urlunparse(components)
+        return new_url
+
+
 class AzureSearchStorage(AzureStorage):
     account_name = getattr(settings, "AZURE_SEARCH_STORAGE_ACCOUNT_NAME", None)
     account_key = getattr(settings, "AZURE_SEARCH_ACCESS_KEY", None)
@@ -338,3 +368,13 @@ class SearchStorage(LazyObject):
 
 
 search_storage = SearchStorage()
+
+
+class DocStorage(LazyObject):
+    def _setup(self):
+        self._wrapped = get_storage_class(
+            getattr(settings, "DOC_STORAGE_CLASS", settings.DEFAULT_FILE_STORAGE)
+        )()
+
+
+doc_storage = DocStorage()
