a18bfe2d114a7ade5176346f36c18ade271322b6
Tom Paterson <tom.paterson@bjss.com>
2024-03-08 16:03:39 +0000

    CV-1206: fix publisher_la 3

--- terraform/src/common/publisher_logic_app.tf
+++ terraform/src/common/publisher_logic_app.tf
@@ -1,10 +1,12 @@
 resource "azapi_resource" "publisher_la" {
-  type      = "Microsoft.Logic/workflows@2019-05-01"
+  type      = "Microsoft.Logic/workflows@2017-07-01"
   name      = replace(data.azurerm_resource_group.rg.name, "-rg-", "-scheduler-la-")
   location  = data.azurerm_resource_group.rg.location
   parent_id = data.azurerm_resource_group.rg.id
   tags      = local.common_tags
   body = jsonencode({
+    "type" : "Microsoft.Logic/workflows",
+    "apiVersion" : "2017-07-01",
     "identity" : {
       "type" : "SystemAssigned"
     },
@@ -60,26 +62,27 @@ resource "azapi_resource" "publisher_la" {
               "method" : "GET",
               "uri" : "${var.publishing_endpoint}"
             }
-          },
-          "outputs" : {}
+          }
         },
-        "parameters" : {
-          "$connections" : {
-            "value" : {
-              "keyvault" : {
-                "connectionId" : "${azapi_resource.keyvault_con.id}",
-                "connectionName" : "${azapi_resource.keyvault_con.name}",
-                "connectionProperties" : {
-                  "authentication" : {
-                    "type" : "ManagedServiceIdentity"
-                  }
-                },
-                "id" : "${data.azurerm_managed_api.kv.id}"
-              }
+        "outputs" : {}
+      },
+      "parameters" : {
+        "$connections" : {
+          "value" : {
+            "keyvault" : {
+              "connectionId" : "${azapi_resource.keyvault_con.id}",
+              "connectionName" : "${azapi_resource.keyvault_con.name}",
+              "connectionProperties" : {
+                "authentication" : {
+                  "type" : "ManagedServiceIdentity"
+                }
+              },
+              "id" : "${data.azurerm_managed_api.kv.id}"
             }
           }
         }
       }
     }
-  })
+    }
+  )
 }
