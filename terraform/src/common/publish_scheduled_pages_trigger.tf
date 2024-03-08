36847eb36418e08024e9f7bb96b62dfd5b9bed79
Tom Paterson <tom.paterson@bjss.com>
2024-03-08 10:38:54 +0000

    CV-1206: correct logic app id

--- terraform/src/common/publish_scheduled_pages_trigger.tf
+++ terraform/src/common/publish_scheduled_pages_trigger.tf
@@ -1,6 +1,6 @@
 resource "azurerm_logic_app_trigger_recurrence" "publish_scheduled_pages_trigger" {
   name         = "Publish scheduled pages trigger"
-  logic_app_id = azurerm_logic_app_workflow.logic_app_workflow.key_vault_id
-  interval = 15
-  frequency = "Minute"
+  logic_app_id = azurerm_logic_app_workflow.publish_scheduled_pages_workflow.id
+  interval     = 15
+  frequency    = "Minute"
 }
