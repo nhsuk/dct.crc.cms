7c7fae5ac5b2693b5878132b850b87ff6da6f54b
Tom Paterson <tom.paterson@bjss.com>
2024-03-11 13:39:55 +0000

    CV-1206: restore access policy and add role assignment

--- terraform/src/common/publish_scheduled_pages_request.tf
+++ terraform/src/common/publish_scheduled_pages_request.tf
@@ -3,5 +3,6 @@ resource "azurerm_logic_app_action_http" "publish_scheduled_pages_request" {
   logic_app_id = azurerm_logic_app_workflow.publish_scheduled_pages_workflow.id
   method       = "GET"
   uri          = var.publishing_endpoint
-  headers      = { Authorization = data.azurerm_key_vault_secret.pubToken.value }
+  headers      = { Authorization = "Bearer ${data.azurerm_key_vault_secret.pubToken.value}" }
+  depends_on   = [azurerm_key_vault_access_policy.terraform_sp_access, azurerm_role_assignment.kv_reader]
 }
