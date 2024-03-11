1a6fd16e91eed454599fd97af7ba68da7545d308
Tom Paterson <tom.paterson@bjss.com>
2024-03-11 14:23:08 +0000

    CV-1206: test for access policy and role assignment creation

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -15,7 +15,6 @@ resource "azurerm_key_vault_access_policy" "terraform_sp_access" {
   key_vault_id = data.azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
-
   secret_permissions = [
     "Get"
   ]
