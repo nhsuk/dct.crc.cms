e2e6ef5ccf0ef84a1f395c511d134811c2b1d2bf
Tom Paterson <tom.paterson@bjss.com>
2024-03-11 13:22:51 +0000

    CV-1206: remove access policy

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -5,18 +5,7 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = var.key_vault_rg
 }
 
-resource "azurerm_key_vault_access_policy" "terraform_sp_access" {
-  key_vault_id = data.azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get"
-  ]
-}
-
 data "azurerm_key_vault_secret" "pubToken" {
   name         = "pubToken"
   key_vault_id = data.azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.terraform_sp_access]
 }
