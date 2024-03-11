38395e4cdd635bdb118f720248106eae644e974e
Tom Paterson <tom.paterson@bjss.com>
2024-03-11 15:27:04 +0000

    CV-1206: remove depends_on for key vault steps done in azure portal

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -8,5 +8,4 @@ data "azurerm_key_vault" "kv" {
 data "azurerm_key_vault_secret" "pubToken" {
   name         = "pubToken"
   key_vault_id = data.azurerm_key_vault.kv.id
-  depends_on   = [azurerm_key_vault_access_policy.terraform_sp_access, azurerm_role_assignment.kv_reader]
 }
