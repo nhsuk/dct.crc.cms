621aeea39a513b506b57b38909c30fc67983ff53
Tom Paterson <tom.paterson@bjss.com>
2024-03-12 10:59:59 +0000

    CV-1206: remove access and role from terraform - needs to be done manually

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -5,24 +5,7 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = var.key_vault_rg
 }
 
-resource "azurerm_key_vault_access_policy" "sp_keyvault_permissions" {
-  key_vault_id = data.azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get"
-  ]
-}
-
-resource "azurerm_role_assignment" "sp_keyvault_role" {
-  scope                = data.azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Reader"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
 data "azurerm_key_vault_secret" "pubToken" {
   name         = "pubToken"
   key_vault_id = data.azurerm_key_vault.kv.id
-  depends_on   = [azurerm_role_assignment.sp_keyvault_role, azurerm_key_vault_access_policy.sp_keyvault_permissions]
 }
