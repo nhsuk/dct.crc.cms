0cf9f17c4836e73c54f90c6a4a2d64b50fb4bea3
hassanj-bjss <129750042+hassanj-bjss@users.noreply.github.com>
2024-03-12 10:10:55 +0000

    [CV-1220] Paragon users search fix (#319)
    * Fixed offset not getting calculated properly.
    
    * Extraced UsersWrapper class to a seprate file
    
    * Added unit tests
    
    * Revert "Added unit tests"
    
    This reverts commit 0c77ffe8acf0c7ef0b6f9ffe698e39fd344fc3fc.

--- campaignresourcecentre/paragon_users/admin_views.py
+++ campaignresourcecentre/paragon_users/admin_views.py
@@ -18,60 +18,9 @@ from campaignresourcecentre.paragon.exceptions import (
     ParagonClientError,
     ParagonClientTimeout,
 )
-from campaignresourcecentre.paragon.models import ParagonCacheValues
 
 from .forms import UserAdminForm, AdminPasswordSetForm
-
-
-class UsersWrapper:
-    """
-    Wrapper class to provide compatability with pagination_nav.html template
-    This template expects the following attributes to exist:
-    number - current page number,
-    paginator - this should provide a sub-attrbute of num_pages
-    has_previous, previous_page_number, has_next, next_page_number
-    """
-
-    def __init__(self, users, num_pages, page_number):
-        self.users = users
-        self.num_pages = num_pages
-        self.page_number = page_number
-
-    def number(self):
-        return self.page_number
-
-    def paginator(self):
-        return {"num_pages": self.num_pages}
-
-    def has_previous(self):
-        if self.page_number > 1:
-            return True
-        else:
-            return False
-
-    def previous_page_number(self):
-        if self.page_number > 1:
-            return self.page_number - 1
-        else:
-            return 1
-
-    def has_next(self):
-        if self.page_number < self.num_pages:
-            return True
-        else:
-            return False
-
-    def next_page_number(self):
-        if self.page_number < self.num_pages:
-            return self.page_number + 1
-        else:
-            return self.num_pages
-
-    def __getitem__(self, key):
-        return self.users[key]
-
-    def __len__(self):
-        return len(self.users)
+from .helpers.UsersPaginationWrapper import UsersPaginationWrapper as UsersWrapper
 
 
 @any_permission_required(
@@ -105,21 +54,8 @@ def search_users(request):
         form = SearchForm(placeholder=_("Email, first name & last name"))
 
     users_per_page = int(request.GET.get("limit", 20))
-    page_num = 1
-
-    if request.GET.get("p"):
-        page_num = int(request.GET.get("p", 1))
-
-    # Fetch current number of users from cache
-    # num_users = 0;
-
-    # if not is_searching:
-
-    #     paragon_cache_values = ParagonCacheValues.load()
-    #     num_users = paragon_cache_values.num_users
-
-    # num_pages = ceil(num_users / users_per_page)
-    offset = page_num * users_per_page
+    page_num = int(request.GET.get("p", 1))
+    offset = users_per_page - (page_num * users_per_page)
 
     try:
         response = paragon_client.search_users(
