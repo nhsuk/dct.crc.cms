c1ade9af7e834d8ef9f04b6ac960e7235b572966
Tom Paterson <tom.paterson@bjss.com>
2024-03-13 12:36:16 +0000

    CV-1206: don't pass pub_url ref into slack template

--- terraform/src/common/scheduler_alert_logic_app.tf
+++ terraform/src/common/scheduler_alert_logic_app.tf
@@ -63,7 +63,7 @@ resource "azapi_resource" "scheduler_alert_la" {
           },
           "Send slack alert" : {
             "inputs" : {
-              "body" : templatefile("${path.module}/templates/slack-alert-body.json.tftpl", { rg_name = data.azurerm_resource_group.rg.name, rg_id = data.azurerm_resource_group.rg.id, la_name = azapi_resource.scheduler_la.name, la_id = azapi_resource.scheduler_la.id, pub_url = "@{body('Get publishing endpoint')?['value']}" }),
+              "body" : templatefile("${path.module}/templates/slack-alert-body.json.tftpl", { rg_name = data.azurerm_resource_group.rg.name, rg_id = data.azurerm_resource_group.rg.id, la_name = azapi_resource.scheduler_la.name, la_id = azapi_resource.scheduler_la.id }),
               "headers" : {
                 "Content-Type" : "application/json"
               },
