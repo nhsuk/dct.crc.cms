a49160a6b5fa28374c53b927c52c16318a7c2705
Tom Paterson <tom.paterson@bjss.com>
2024-03-13 12:14:37 +0000

    CV-1206: use data.azapi_resource_action.scheduler_alert_la_callbackurl.output

--- terraform/src/common/scheduler_alert_action_group.tf
+++ terraform/src/common/scheduler_alert_action_group.tf
@@ -6,7 +6,7 @@ resource "azurerm_monitor_action_group" "scheduler_alert" {
   logic_app_receiver {
     name                    = "scheduler slack alert"
     resource_id             = azapi_resource.scheduler_alert_la.id
-    callback_url            = jsonencode(data.azapi_resource_action.scheduler_alert_la_callbackurl.output).value
+    callback_url            = data.azapi_resource_action.scheduler_alert_la_callbackurl.output
     use_common_alert_schema = true
   }
 }
