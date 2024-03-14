6c58e395259b231416ae5c288298e5355238378b
Tom Paterson <tom.paterson@bjss.com>
2024-03-14 10:54:59 +0000

    CV-1206: reduce redundant requests

--- terraform/src/common/scheduler_logic_app.tf
+++ terraform/src/common/scheduler_logic_app.tf
@@ -20,15 +20,78 @@ resource "azapi_resource" "scheduler_la" {
             "type" : "Object"
           }
         },
+
         "triggers" : {
           "Publish scheduled pages trigger" : {
             "evaluatedRecurrence" : {
-              "frequency" : "Minute",
-              "interval" : 15
+              "frequency" : "Day",
+              "interval" : 1,
+              "schedule" : {
+                "hours" : [
+                  "0",
+                  "1",
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "15",
+                  "16",
+                  "17",
+                  "18",
+                  "19",
+                  "20",
+                  "21",
+                  "22",
+                  "23"
+                ],
+                "minutes" : [
+                  5
+                ]
+              }
             },
             "recurrence" : {
-              "frequency" : "Minute",
-              "interval" : 15
+              "frequency" : "Day",
+              "interval" : 1,
+              "schedule" : {
+                "hours" : [
+                  "0",
+                  "1",
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "15",
+                  "16",
+                  "17",
+                  "18",
+                  "19",
+                  "20",
+                  "21",
+                  "22",
+                  "23"
+                ],
+                "minutes" : [
+                  5
+                ]
+              }
             },
             "type" : "Recurrence"
           }
