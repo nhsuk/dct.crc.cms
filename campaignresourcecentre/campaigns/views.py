de2aee6d15a754b51941895fcb7b9c04635a2280
Calvin Merry <104705692+CodeByCalvin@users.noreply.github.com>
2024-03-25 16:36:46 +0000

    Removed repeated logic from views.py that is available in CampaignHubPage/model.py

--- campaignresourcecentre/campaigns/views.py
+++ campaignresourcecentre/campaigns/views.py
@@ -6,14 +6,10 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET"])
 def render_topic(request):
     campaign_hub_page = campaign.objects.first()
-
-    if request.GET.get("sort") == "recommended":
-        campaigns = campaign_hub_page.from_database(request)
-    else:
-        campaigns = campaign_hub_page.from_azure_search(request)
+    context = campaign_hub_page.get_context(request)
 
     return render(
         request,
         "molecules/campaigns/campaigns.html",
-        {"request": request, "campaigns": campaigns},
+        context,
     )
