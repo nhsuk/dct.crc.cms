8f089861b72136ea4742df13cd10416b166a4412
Tom Paterson <tom.paterson@bjss.com>
2024-06-03 08:06:15 +0000

    CV-1240: skip empty files in coverage reports

--- docker/coverage.sh
+++ docker/coverage.sh
@@ -13,13 +13,13 @@ echo "[4/9] Mounting test results..."
 mv ./testresults.xml ./docker
 
 echo "[5/9] Generating xml coverage report..."
-coverage xml -i
+coverage xml -i --skip-empty
 
 echo "[6/9] Mounting xml coverage report..."
 mv ./coverage.xml ./docker
 
 echo "[7/9] Generating html coverage report..."
-coverage html -i -d coverage_html
+coverage html -i --skip-empty -d coverage_html
 
 echo "[8/9] Mounting html coverage report..."
 mkdir ./docker/coverage_html
