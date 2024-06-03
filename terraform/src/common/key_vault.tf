b81d338642107f5db9cca527562aca0752cc45de
Hassan Jalil <hassan.jalil@bjss.com>
2024-06-03 16:49:35 +0100

    terraform test #5

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -9,63 +9,27 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled    = false
   sku_name                    = "standard"
 
-  # access_policy {
-  #   tenant_id = azapi_resource.scheduler_la.identity[0].tenant_id
-  #   object_id = azapi_resource.scheduler_la.identity[0].principal_id
-  #   secret_permissions = [
-  #     "Get", "List"
-  #   ]
-  # }
+  access_policy {
+    tenant_id = azapi_resource.scheduler_la.identity[0].tenant_id
+    object_id = azapi_resource.scheduler_la.identity[0].principal_id
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
+  
+  access_policy {
+    tenant_id = azapi_resource.scheduler_alert_la.identity[0].tenant_id
+    object_id = azapi_resource.scheduler_alert_la.identity[0].principal_id
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
 
-  # access_policy {
-  #   tenant_id = azapi_resource.scheduler_alert_la.identity[0].tenant_id
-  #   object_id = azapi_resource.scheduler_alert_la.identity[0].principal_id
-  #   secret_permissions = [
-  #     "Get", "List"
-  #   ]
-  # }
-
-  # access_policy {
-  #   tenant_id = azapi_resource.search_reindex.identity[0].tenant_id
-  #   object_id = azapi_resource.search_reindex.identity[0].principal_id
-  #   secret_permissions = [
-  #     "Get", "List"
-  #   ]
-  # }
-}
-
-resource "azurerm_key_vault_access_policy" "scheduler_la" {
-  key_vault_id = azurerm_key_vault.current.id
-
-  tenant_id = azapi_resource.scheduler_la.identity[0].tenant_id
-  object_id = azapi_resource.scheduler_la.identity[0].principal_id
-
-  secret_permissions = [
-    "GET",
-    "LIST"
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "scheduler_alert_la" {
-  key_vault_id = azurerm_key_vault.current.id
-
-  tenant_id = azapi_resource.scheduler_alert_la.identity[0].tenant_id
-  object_id = azapi_resource.scheduler_alert_la.identity[0].principal_id
-
-  secret_permissions = [
-    "GET",
-    "LIST"
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "search_reindex" {
-  key_vault_id = azurerm_key_vault.current.id
-
-  tenant_id = azapi_resource.search_reindex.identity[0].tenant_id
-  object_id = azapi_resource.search_reindex.identity[0].principal_id
-
-  secret_permissions = [
-    "GET",
-    "LIST"
-  ]
+  access_policy {
+    tenant_id = azapi_resource.search_reindex.identity[0].tenant_id
+    object_id = azapi_resource.search_reindex.identity[0].principal_id
+    secret_permissions = [
+      "Get", "List"
+    ]
+  }
 }
