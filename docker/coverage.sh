852e274cb7a9a3c6b9da90563da4c05e5e3dcf3f
Tom Paterson <tom.paterson@bjss.com>
2024-06-10 22:11:37 +0000

    CV-1240: improve code coverage report

--- docker/coverage.sh
+++ docker/coverage.sh
@@ -13,13 +13,13 @@ echo "[4/9] Mounting test results..."
 mv ./testresults.xml ./docker
 
 echo "[5/9] Generating xml coverage report..."
-coverage xml -i --skip-empty
+coverage xml
 
 echo "[6/9] Mounting xml coverage report..."
 mv ./coverage.xml ./docker
 
 echo "[7/9] Generating html coverage report..."
-coverage html -i --skip-empty -d coverage_html
+coverage html
 
 echo "[8/9] Mounting html coverage report..."
 mkdir ./docker/coverage_html
