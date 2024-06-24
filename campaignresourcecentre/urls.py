533a61c631db6ca6398a17000b75a39cb7191e5f
Hassan Jalil <hassan.jalil@bjss.com>
2024-06-24 17:32:53 +0100

    Reverted cv-1226

--- campaignresourcecentre/urls.py
+++ campaignresourcecentre/urls.py
@@ -42,7 +42,6 @@ from campaignresourcecentre.standardpages.views import (
     cookie_settings,
     cookie_declaration,
     update_index,
-    debug_az_search,
     search_orphans,
     manage_files,
 )
@@ -58,8 +57,6 @@ private_urlpatterns = [
     path("crc-admin/password_reset/", user_admin_views.PasswordResetView.as_view()),
     path("crc-admin/clear_cache/", clear_cache, name="clear_cache"),
     path("crc-admin/update_index/", update_index, name="update_index"),
-    path("crc-admin/update_index/", update_index, name="update_index"),
-    path("crc-admin/dump_az_search_index/", debug_az_search, name="debug_az_index"),
     path("crc-admin/search_orphans/", search_orphans, name="search_orphans"),
     path("crc-admin/manage_files/", manage_files, name="manage_files"),
     path("crc-admin/", include(wagtailadmin_urls)),
