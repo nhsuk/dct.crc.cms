a0017bbe92effaa0e5fc611949d0bde043868ae0
Calvin Merry <104705692+CodeByCalvin@users.noreply.github.com>
2024-06-28 15:00:22 +0000

    Fixed formatting

--- terraform/src/common/provider.tf
+++ terraform/src/common/provider.tf
@@ -8,6 +8,7 @@ provider "azurerm" {
   skip_provider_registration = true  
   features {}  
   subscription_id = "07748954-52d6-46ce-95e6-2701bfc715b4"  # nhsuk-development 
+}
 
 provider "azurerm" {  
   alias = "nhsuk-staging"  
