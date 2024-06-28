5261ada95de23aabd578a4b75ed30fb77d72bac8
Calvin Merry <104705692+CodeByCalvin@users.noreply.github.com>
2024-06-28 14:57:30 +0000

    Updated provider subscription id

--- terraform/src/common/provider.tf
+++ terraform/src/common/provider.tf
@@ -7,21 +7,20 @@ provider "azurerm" {
   alias = "nhsuk-integration"  
   skip_provider_registration = true  
   features {}  
-  subscription_id = "2cf44e0d-817d-4596-b471-0788f8a14ab2"  
-}
+  subscription_id = "07748954-52d6-46ce-95e6-2701bfc715b4"  # nhsuk-development 
 
 provider "azurerm" {  
   alias = "nhsuk-staging"  
   skip_provider_registration = true  
   features {}  
-  subscription_id = "b0787d6a-56e3-4899-bc30-723f9d78899c"  
+  subscription_id = "b0787d6a-56e3-4899-bc30-723f9d78899c"   # nhsuk-staging 
 }
 
 provider "azurerm" {  
   alias = "nhsuk-production"  
   skip_provider_registration = true  
   features {}  
-  subscription_id = "1e543650-5458-44ea-a3b1-35a6d0d92cc9"  
+  subscription_id = "1e543650-5458-44ea-a3b1-35a6d0d92cc9"   # nhsuk (production)
 }
 
 provider "azapi" {  
