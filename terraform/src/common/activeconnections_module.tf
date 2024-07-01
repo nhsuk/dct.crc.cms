656e4ba9ccdbf0253e2c9e7c4d92fb40144cc717
Calvin Merry <104705692+CodeByCalvin@users.noreply.github.com>
2024-07-01 08:34:13 +0000

    Removed stag/prod providers as not needed

--- terraform/src/common/activeconnections_module.tf
+++ terraform/src/common/activeconnections_module.tf
@@ -14,23 +14,3 @@ module "nhsuk-integration" {
     azurerm = azurerm.nhsuk-integration  
   }  
 }
-
-module "nhsuk-staging" {  
-  count       = var.environment == "staging" ? 1 : 0  
-  source      = "./modules/activeconnectionsalert"  
-  environment = var.environment  
-  location    = var.location  
-  providers = {  
-    azurerm = azurerm.nhsuk-staging  
-  }  
-}
-
-module "nhsuk-production" {  
-  count       = var.environment == "production" ? 1 : 0  
-  source      = "./modules/activeconnectionsalert"  
-  environment = var.environment  
-  location    = var.location  
-  providers = {  
-    azurerm = azurerm.nhsuk-production  
-  }  
-}
--- terraform/src/common/provider.tf
+++ terraform/src/common/provider.tf
@@ -10,19 +10,5 @@ provider "azurerm" {
   subscription_id = "07748954-52d6-46ce-95e6-2701bfc715b4"  # nhsuk-development 
 }
 
-provider "azurerm" {  
-  alias = "nhsuk-staging"  
-  skip_provider_registration = true  
-  features {}  
-  subscription_id = "b0787d6a-56e3-4899-bc30-723f9d78899c"   # nhsuk-staging 
-}
-
-provider "azurerm" {  
-  alias = "nhsuk-production"  
-  skip_provider_registration = true  
-  features {}  
-  subscription_id = "1e543650-5458-44ea-a3b1-35a6d0d92cc9"   # nhsuk (production)
-}
-
 provider "azapi" {  
 }
