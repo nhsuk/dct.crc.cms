72997a9868adfe9e0beb63561577d4456c647b0b
Dylan Jones <dylan.jones12@nhs.net>
2024-11-04 16:39:29 +0000

    Document why there is an override

--- terraform/src/common/provider.tf
+++ terraform/src/common/provider.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
   alias                           = "database"
   resource_provider_registrations = "none"
   features {}
+  # Integration database is in nhsuk-development subscription
   subscription_id = var.environment == "integration" ? "07748954-52d6-46ce-95e6-2701bfc715b4" : null
 }
 
