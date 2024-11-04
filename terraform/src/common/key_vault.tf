8bf87f95e7c9cd2499d583eb7a01eb615dfe779d
Dylan Jones <dylan.jones12@nhs.net>
2024-11-04 09:52:03 +0000

    Add flexible postgresql server with optional replica

--- terraform/src/common/key_vault.tf
+++ terraform/src/common/key_vault.tf
@@ -70,3 +70,15 @@ resource "azurerm_key_vault_secret" "secrets" {
     ignore_changes = [value]
   }
 }
+
+resource "azurerm_key_vault_secret" "postgresql_admin_user" {
+  name         = "postgresqlAdminUser"
+  value        = azurerm_postgresql_flexible_server.database.administrator_login
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "postgresql_admin_password" {
+  name         = "postgresqlAdminPassword"
+  value        = azurerm_postgresql_flexible_server.database.administrator_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
