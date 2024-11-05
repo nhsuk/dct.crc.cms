980c256fe251f4fc137ff492b85a78493af15a45
Dylan Jones <dylan.jones12@nhs.net>
2024-11-05 12:12:07 +0000

    [CV-1275] Deploy flexible posgresql (#365)
    * Swap deprecated param to new resource_provider_registrations
    * Add flexible postgresql server with optional replica
    * Remove requirement to have a dev provider for higher environments
    * Document why there is an override

--- terraform/src/common/locals.tf
+++ terraform/src/common/locals.tf
@@ -13,21 +13,43 @@ locals {
   scheduler_logic_app_name         = replace(data.azurerm_resource_group.rg.name, "-rg-", "-scheduler-la-")
   search_reindex_logic_app_name    = replace(data.azurerm_resource_group.rg.name, "-rg-", "-search-reindex-la-")
   key_vault_name                   = replace(data.azurerm_resource_group.rg.name, "-rg-", "-kv-")
+  postgres_flex_name               = replace(data.azurerm_resource_group.rg.name, "-rg-", "-psql-")
   activeconnections_logic_app_name = replace(data.azurerm_resource_group.rg.name, "-rg-", "-activeconnectionsalert-la-")
   activeconnections_logic_app_id   = "${data.azurerm_resource_group.rg.id}/providers/Microsoft.Logic/workflows/${local.activeconnections_logic_app_name}"
 
-  activeconnectionsalert_environment_map = {
-    development = module.activeconnectionsalert
-    integration = module.activeconnectionsalert-integration
-    staging     = module.activeconnectionsalert
-    production  = module.activeconnectionsalert
+  postgresql_server_resource_id = module.activeconnectionsalert.postgresql_server_id
+  postgresql_server_name        = module.activeconnectionsalert.postgresql_server_name
+  postgresql_server_url         = "https://portal.azure.com/#@nhschoices.net/resource${local.postgresql_server_resource_id}/overview"
+
+  database_skus = {
+    development = "GP_Standard_D2s_v3" # General Purpose, 2 vCores, 8 GiB RAM
+    integration = "GP_Standard_D2s_v3" # General Purpose, 2 vCores, 8 GiB RAM
+    staging     = "GP_Standard_D2s_v3" # General Purpose, 2 vCores, 8 GiB RAM
+    production  = "GP_Standard_D2s_v3" # General Purpose, 2 vCores, 8 GiB RAM
   }
 
-  selected_environment = lookup(local.activeconnectionsalert_environment_map, var.environment)[0]
+  database_sku = local.database_skus[var.environment]
 
-  postgresql_server_resource_id = local.selected_environment.postgresql_server_id
-  postgresql_server_name        = local.selected_environment.postgresql_server_name
-  postgresql_server_url         = "https://portal.azure.com/#@nhschoices.net/resource${local.postgresql_server_resource_id}/overview"
+  database_firewall_rules = {
+    AllowAllAzure = {
+      start_ip = "0.0.0.0"
+      end_ip   = "0.0.0.0"
+    }
+  }
+
+  database_replicas = {
+    production = {
+      name           = "dct-crccms-psql-prod-ukw"
+      resource_group = "dct-crccms-rg-prod-ukw"
+      location       = "ukwest"
+    }
+  }
+
+  database_replica = lookup(local.database_replicas, var.environment, null)
+
+  databases = [
+    "crc"
+  ]
 
   secret_names = [
     "alertingWebhook",
--- terraform/src/common/provider.tf
+++ terraform/src/common/provider.tf
@@ -1,13 +1,14 @@
 provider "azurerm" {
-  skip_provider_registration = true
+  resource_provider_registrations = "none"
   features {}
 }
 
 provider "azurerm" {
-  alias                      = "nhsuk-development"
-  skip_provider_registration = true
+  alias                           = "database"
+  resource_provider_registrations = "none"
   features {}
-  subscription_id = "07748954-52d6-46ce-95e6-2701bfc715b4"
+  # Integration database is in nhsuk-development subscription
+  subscription_id = var.environment == "integration" ? "07748954-52d6-46ce-95e6-2701bfc715b4" : null
 }
 
 provider "azapi" {
