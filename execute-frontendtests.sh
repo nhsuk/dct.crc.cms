7938ebe63bfb7b4b98c73167deb3338c40c062f7
Dylan Jones <dylan.jones12@nhs.net>
2024-11-26 10:07:07 +0000

    Inject basic auth credentials to staging URL (#374)
    Staging is behind basic auth and tests are currently failing with a 401

--- execute-frontendtests.sh
+++ execute-frontendtests.sh
@@ -16,12 +16,20 @@
 # IMAGE_TAG: Tag for required version of the Front End test image
 # PARALLEL - number of parallel runs to execute (default 1)
 # SCENARIOS - how to run scenarios (sequential/parallel, default sequential)
+# AUTHORIZE - optional env variable name containing basic auth credentials
 
 echo "### IMAGE_TAG: ${IMAGE_TAG:=1.1.1}"
 echo "### PARALLEL: ${PARALLEL:=1}"
 echo "### SCENARIOS: ${SCENARIOS:=sequential}"
 echo "### TIMEOUT: ${TIMEOUT:=3000}s"
-
+echo "### AUTHORIZE: ${AUTHORIZE:=None}"
+if [ "$AUTHORIZE" == "None" ]
+then
+  AUTH_URL=$BASE_URL
+else
+  AUTH_URL="https://${!AUTHORIZE}@${BASE_URL#https://}"
+  echo "Authorization inserted into BASE_URL as $AUTH_URL"
+fi
 # Can't have empty value for TAGS as pipeline parameter so change "all" to "" here
 TAGS=$([ "$TAGS" = "all" ] && echo "" || echo "$TAGS")
 echo "Effective TAGS: '$TAGS'"
@@ -41,23 +49,23 @@ docker build --build-arg IMAGE_TAG=${IMAGE_TAG} -t my-acceptancetests:${IMAGE_TA
 
 printf  'Docker Build completed'
 
-# Wait for the BASE_URL to be available or timeout after one minute
+# Wait for the AUTH_URL to be available or timeout after one minute
 
 for i in {1, 6}
 do
-  curl --output /dev/null -k --head --fail --max-time 10 ${BASE_URL} && break
+  curl --output /dev/null -k --head --fail --max-time 10 ${AUTH_URL} && break
   printf 'Failed to access server, trying again in 10 seconds...\n'
   sleep 10
 done
 printf  'Server seems to be responding - confirm\n'
 # Confirm site now running or fail
-curl --output /dev/null -k --head --fail --max-time 10 ${BASE_URL} || exit 1
+curl --output /dev/null -k --head --fail --max-time 10 ${AUTH_URL} || exit 1
 printf  'Server responded. Running tests now\n'
 
 # get Docker to use host network so it can access localhost:8000 with no fuss
 docker run \
   --network host \
-  --env BASE_URL \
+  --env BASE_URL=$AUTH_URL \
   --env TAGS \
   --env PARALLEL=$PARALLEL \
   --env SCENARIOS=$SCENARIOS \
