2fc2ee7eeb81a2e521248f2d2a90af1ff11f5beb
Dylan Jones <dylan.jones12@nhs.net>
2025-06-12 20:04:41 +0100

    Point to tagged module

--- terraform/src/common/container_app_environment.tf
+++ terraform/src/common/container_app_environment.tf
@@ -1,5 +1,5 @@
 module "container_app_env" {
-  source = "git::https://github.com/nhsuk/dct.terraform-modules.container-app-env?ref=main"
+  source = "git::https://github.com/nhsuk/dct.terraform-modules.container-app-env?ref=1.0.0"
 
   count = var.deploy_container_apps ? 1 : 0
 
