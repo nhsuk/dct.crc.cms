b48642d5fff772831cf9d5fb58db6aac6aec0fdd
Dylan Jones <dylan.jones12@nhs.net>
2025-06-19 16:51:43 +0100

    [CV-1321] Add Network security group and resource tags (#438)
    * Add nsg rules for subnet, bump cae with tags policy fix
    
    * Remove un-needed conditional logic on hub network subscription
    
    All managed identities now have access

--- terraform/src/common/network.tf
+++ terraform/src/common/network.tf
@@ -24,6 +24,118 @@ module "network_spoke" {
           ]
         }
       }
+      nsg_rules = {
+        Allow80Inbound = {
+          priority                   = 100
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "80"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        Allow31080Inbound = {
+          priority                   = 110
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "31080"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        Allow443Inbound = {
+          priority                   = 120
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        Allow31443Inbound = {
+          priority                   = 130
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "31443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "*"
+        },
+        AllowAzureLoadBalancerInbound = {
+          priority                   = 140
+          direction                  = "Inbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "30000-32767"
+          source_address_prefix      = "AzureLoadBalancer"
+          destination_address_prefix = "*"
+        },
+        AllowACROutbound = {
+          priority                   = 150
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "MicrosoftContainerRegistry"
+        },
+        AllowAzureFrontDoorOutbound = {
+          priority                   = 160
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "AzureFrontDoor.FirstParty"
+        },
+        AllowAADOutbound = {
+          priority                   = 170
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "AzureActiveDirectory"
+        },
+        AllowDNSOutbound = {
+          priority                   = 180
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "53"
+          source_address_prefix      = "*"
+          destination_address_prefix = "168.63.129.16"
+        },
+        AllowStorageUKSOutbound = {
+          priority                   = 190
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "Storage.uksouth"
+        }
+        AllowStorageUKWOutbound = {
+          priority                   = 200
+          direction                  = "Outbound"
+          access                     = "Allow"
+          protocol                   = "Tcp"
+          source_port_range          = "*"
+          destination_port_range     = "443"
+          source_address_prefix      = "*"
+          destination_address_prefix = "Storage.ukwest"
+        }
+      }
     }
   }
 
