671d1d672ce21270be7ac3c96a8f0ce80eb0311c
Ele Taylor <ele.taylor1@nhs.net>
2025-08-05 17:28:56 +0100

    add law again

--- terraform/src/common/modules/postgresql_flexible_server/data.tf
+++ terraform/src/common/modules/postgresql_flexible_server/data.tf
@@ -1,1 +1,9 @@
 data "azurerm_client_config" "current" {}
+
+data "azurerm_log_analytics_workspace" "shared_log_analytics_workspace" {
+  count    = 1
+  provider = azurerm.law
+
+  name                = local.law_name
+  resource_group_name = local.law_resource_group_name
+}
