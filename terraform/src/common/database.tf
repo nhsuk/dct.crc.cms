99e61d291b131f0338bbfc0e812c00ab052081cc
Ele Taylor <ele.taylor1@nhs.net>
2025-08-05 16:50:43 +0100

    Formatting fix

--- terraform/src/common/database.tf
+++ terraform/src/common/database.tf
@@ -1,12 +1,12 @@
 module "database" {
   count = local.deploy_database ? 1 : 0
 
-  source                      = "./modules/postgresql_flexible_server"
-  name                        = local.postgres_flex_name
-  resource_group              = data.azurerm_resource_group.rg
-  environment                 = var.environment
-  key_vault                   = azurerm_key_vault.kv
-  log_analytics_workspace_id  = azurerm_log_analytics_workspace.shared_log_analytics_workspace
+  source                     = "./modules/postgresql_flexible_server"
+  name                       = local.postgres_flex_name
+  resource_group             = data.azurerm_resource_group.rg
+  environment                = var.environment
+  key_vault                  = azurerm_key_vault.kv
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.shared_log_analytics_workspace
 
   providers = {
     azurerm.law = azurerm.law
