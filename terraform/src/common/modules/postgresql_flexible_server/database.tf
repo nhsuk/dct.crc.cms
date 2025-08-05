40e1605302cc9a940012cfd0f8b00339328e85cd
Ele Taylor <ele.taylor1@nhs.net>
2025-08-05 14:33:21 +0100

    Correct var usage

--- terraform/src/common/modules/postgresql_flexible_server/database.tf
+++ terraform/src/common/modules/postgresql_flexible_server/database.tf
@@ -103,7 +103,7 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "replica_firewall_ru
 }
 
 resource "azurerm_monitor_diagnostic_setting" "psql" {
-  name                       = "dct-crc-ds-psql-${var.environment}-${resource_group.location}"
+  name                       = "dct-crc-ds-psql-${var.environment}-${var.resource_group.location}"
   target_resource_id         = azurerm_postgresql_flexible_server.database.id
   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.shared_log_analytics_workspace.id
 
@@ -118,7 +118,7 @@ resource "azurerm_monitor_diagnostic_setting" "psql" {
 
 resource "azurerm_monitor_diagnostic_setting" "replica_psql" {
   count                      = local.database_replica != null ? 1 : 0
-  name                       = "dct-crc-ds-psql-rp-${var.environment}-${resource_group.location}"
+  name                       = "dct-crc-ds-psql-rp-${var.environment}-${var.resource_group.location}"
   target_resource_id         = azurerm_postgresql_flexible_server.replica[0].id
   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.shared_log_analytics_workspace.id
 
