806327cbb6c23c0295b896fc6ba0a314f0051ecd
Ele Taylor <ele.taylor1@nhs.net>
2025-08-06 15:53:01 +0100

    formatting

--- terraform/src/common/modules/postgresql_flexible_server/provider.tf
+++ terraform/src/common/modules/postgresql_flexible_server/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source                = "hashicorp/azurerm"
-        configuration_aliases = [ azurerm.law]
+      source                = "hashicorp/azurerm"
+      configuration_aliases = [ azurerm.law]
     }
   }
 }
