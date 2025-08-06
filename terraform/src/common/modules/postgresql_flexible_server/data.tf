1c3e9e93d2234943a6f635c560b6b030ad6197cf
Ele Taylor <ele.taylor1@nhs.net>
2025-08-06 10:12:03 +0100

    Law test

--- terraform/src/common/modules/postgresql_flexible_server/data.tf
+++ terraform/src/common/modules/postgresql_flexible_server/data.tf
@@ -1,9 +1,1 @@
 data "azurerm_client_config" "current" {}
-
-data "azurerm_log_analytics_workspace" "shared_log_analytics_workspace" {
-  count    = 1
-  provider = azurerm.law
-
-  name                = local.law_name
-  resource_group_name = local.law_resource_group_name
-}
