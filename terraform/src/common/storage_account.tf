d2bee7126ef15c49f98014139f20b64565aa68eb
Ele Taylor <ele.taylor1@nhs.net>
2025-08-07 16:06:03 +0100

    formatting

--- terraform/src/common/storage_account.tf
+++ terraform/src/common/storage_account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "crc_cms_storage_account" {
-  name                     = var.env == "prod" ? "campaignscrcv3produks" : ( var.env == "stag" ? "campaignscrcv3staguks" :"campaignsstrgintuks")
+  name                     = var.env == "prod" ? "campaignscrcv3produks" : (var.env == "stag" ? "campaignscrcv3staguks" :"campaignsstrgintuks")
   resource_group_name      = var.resource_group
   location                 = var.location
   account_tier             = "Standard"
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "crc_cms_storage_account" {
 
 #trivy:ignore:avd-azu-0007 Storage container public access should be on because it serves the images for the website
 resource "azurerm_storage_container" "campaigns_crc" {
-  name                  = var.env == "prod" ? "campaign-resource-centre-v3-production" : ( var.env == "stag" ? "campaign-resource-centre-v3-staging" :( var.env == "int" ? "campaign-resouce-centre-v3-integration" :"campaign-resouce-centre-v3-review"))
+  name                  = var.env == "prod" ? "campaign-resource-centre-v3-production" : (var.env == "stag" ? "campaign-resource-centre-v3-staging" :(var.env == "int" ? "campaign-resouce-centre-v3-integration" :"campaign-resouce-centre-v3-review"))
   storage_account_id    = azurerm_storage_account.crc_cms_storage_account.id
   container_access_type = "blob"
 }
