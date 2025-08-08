fa7a4e2065404280bef2e5284b1cdcb2fd898c46
Ele Taylor <ele.taylor1@nhs.net>
2025-08-08 13:32:06 +0100

    formatting

--- campaignresourcecentre/settings/base.py
+++ campaignresourcecentre/settings/base.py
@@ -697,6 +697,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = True
 # expires cookies after 30 mins
 SESSION_COOKIE_AGE = 3600
+
 # Enabled as it was identified in Pen test as a problem
 # https://docs.djangoproject.com/en/5.2/ref/settings/#session-cookie-httponly
 SESSION_COOKIE_HTTPONLY = True
