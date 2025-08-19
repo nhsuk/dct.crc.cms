5ad8d09eff22239bd416e0dac12600afea1bab7c
Ele Taylor <ele.taylor1@nhs.net>
2025-08-19 12:30:12 +0100

    Update for_each

--- terraform/src/common/storage_account.tf
+++ terraform/src/common/storage_account.tf
@@ -9,15 +9,16 @@ locals {
 }
 
 import {
-  for_each = local.locs
+  for_each = local.locs["${var.env}${var.location}"]
   to       = azurerm_storage_account.crc_cms
-  id       = "${data.azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/each.value"
+  id       = "${data.azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${each.value}"
 }
 
 
 # # Create a storage account
 resource "azurerm_storage_account" "crc_cms" {
-  name                     = "campaignscrcv3strg${var.env}${var.location}"
+  for_each                 = local.locs["${var.env}${var.location}"]
+  name                     = each.value
   resource_group_name      = var.resource_group
   location                 = var.long_location
   account_tier             = "Standard"
