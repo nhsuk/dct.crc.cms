d1938febe94a69be499cd948b35c6bafa901c955
Ele Taylor <ele.taylor1@nhs.net>
2025-08-19 12:48:46 +0100

    Fix for each

--- terraform/src/common/storage_account.tf
+++ terraform/src/common/storage_account.tf
@@ -1,23 +1,23 @@
 locals {
-  locs = {
+  locs ={
     "devuks" : ["campaignscrcv3strgdevuks"]
-    "intuks" : ["campaignsstrgintuks"]
-    "staguks" : ["campaignsstrgstaguks"]
-    "produks" : ["campaignscrcv3strgproduks"]
+    "intuks" : ["campaignscrcv3strgintuks"]
+    "staguks" : ["campaignscrcv3staguks"]
+    "produks" : ["campaignscrcv3produks"]
     "produkw" : []
   }
 }
 
 import {
-  for_each = local.locs["${var.env}${var.location}"]
-  to       = azurerm_storage_account.crc_cms
+  for_each = toset(local.locs["${var.env}${var.location}"])
+  to       = azurerm_storage_account.crc_cms[each.key]
   id       = "${data.azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${each.value}"
 }
 
 
 # # Create a storage account
 resource "azurerm_storage_account" "crc_cms" {
-  for_each                 = local.locs["${var.env}${var.location}"]
+  for_each                 = toset(local.locs["${var.env}${var.location}"])
   name                     = each.value
   resource_group_name      = var.resource_group
   location                 = var.long_location
