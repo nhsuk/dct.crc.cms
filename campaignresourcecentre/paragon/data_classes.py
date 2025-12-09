8c71d87953f1b1c06d9d5243c3d5e005e4343841
Hassan Jalil <129750042+hassanj-bjss@users.noreply.github.com>
2025-12-09 16:31:50 +0000

    [CV-1361] Update data sent to DCX's api (#495)
    * Updated fields being sent to dcx
    
    * removed temporary log message
    
    * reformatted files
    
    * updated tests
    
    * added additional tests
    
    * reformatted files
    
    * added additional coverage
    
    * file formatted
    
    * re-added removed password for paragon
    
    * Added tests for admin_views
    
    * reformatted files
    
    * added tests for signup form
    
    * reformatted files
    
    * (CV-1367) updated job title to job function under user accounts
    
    * Updated field mapping
    
    * reformatted files
    
    * Updated postcode logic and added tests
    
    * Addressed sonarcloud issues
    
    * Update campaignresourcecentre/paragon_users/views.py
    
    Co-authored-by: tompaterson96 <63047467+tompaterson96@users.noreply.github.com>
    
    * updated regex for postcode pattern
    
    Co-authored-by: tompaterson96 <63047467+tompaterson96@users.noreply.github.com>
    
    * Refactored verification method for easier readability & added tests
    
    * Refactor get_postcode_data function for improved error handling
    
    * Reformatted files
    
    * addressed urllib3 vulnerability: CVE-2025-66418
    
    * regenerated lock file
    
    * remove urllib
    
    ---------
    
    Co-authored-by: tompaterson96 <63047467+tompaterson96@users.noreply.github.com>

--- campaignresourcecentre/paragon/data_classes.py
+++ campaignresourcecentre/paragon/data_classes.py
@@ -16,7 +16,9 @@ class Registration:
     last_name: str
     organisation: str
     job_title: str
+    area_work: str
     postcode: str
+    postcode_region: str
     created_at: datetime
 
     def valid(self):
@@ -33,6 +35,8 @@ class Registration:
             "Password": self.password,
             "FirstName": self.first_name,
             "LastName": self.last_name,
+            "ContactVar2": self.area_work,
+            "ContactVar3": self.postcode_region,
             "ProductRegistrationVar2": "False",  # Is active
             "ProductRegistrationVar3": self.organisation,
             "ProductRegistrationVar4": self.job_title,
@@ -51,12 +55,14 @@ class User:
     last_name: str
     organisation: str
     job_title: str
+    area_work: str
     role: str
     active: str
     created_at: datetime
     verified_at: str
     subscriptions: str
     postcode: str
+    postcode_region: str
 
     @property
     def full_name(self):
@@ -93,6 +99,10 @@ class User:
             result["ProductRegistrationVar9"] = self.postcode
         if self.created_at:
             result["ProductRegistrationVar10"] = self.created_at
+        if self.postcode_region:
+            result["ContactVar3"] = self.postcode_region
+        if self.area_work:
+            result["ContactVar2"] = self.area_work
 
         return result
 
@@ -130,6 +140,8 @@ def user_from_dict(user_dict):
         verified_at=user_dict["ProductRegistrationVar8"],
         subscriptions=user_dict["ProductRegistrationVar7"],
         postcode=user_dict["ProductRegistrationVar9"],
+        postcode_region=user_dict["ContactVar3"],
+        area_work=user_dict["ContactVar2"],
     )
 
 
