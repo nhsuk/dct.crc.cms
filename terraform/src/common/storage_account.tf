d498b208b9450865cae9d73e5da805aa3a11e7f5
Tom Paterson <tompaterson1996@gmail.com>
2026-04-09 12:52:16 +0100

    CV-1426: create dctcrccmsbackupsdevuks storage account in dev for backups and give pipeline blob contributor role

--- terraform/src/common/storage_account.tf
+++ terraform/src/common/storage_account.tf
@@ -20,6 +20,15 @@ resource "azurerm_storage_account" "crc_cms" {
   }
 }
 
+#trivy:ignore:avd-azu-0007 storage container public access is enabled as it serves the assets for the website
+resource "azurerm_storage_container" "crc_cms" {
+  for_each = local.storage_container
+
+  name                  = each.key
+  storage_account_id    = azurerm_storage_account.crc_cms[each.value].id
+  container_access_type = "blob"
+}
+
 resource "azurerm_role_assignment" "storage_blob_contributor_pipeline_identity" {
   for_each = azurerm_storage_account.crc_cms
 
@@ -38,11 +47,42 @@ resource "azurerm_role_assignment" "storage_blob_contributor_apps_identity" {
   principal_type       = "ServicePrincipal"
 }
 
-#trivy:ignore:avd-azu-0007 storage container public access is enabled as it serves the assets for the website
-resource "azurerm_storage_container" "crc_cms" {
-  for_each = local.storage_container
+resource "azurerm_storage_account" "crc_cms_backups" {
+  count = var.env == "dev" ? 1 : 0
 
-  name                  = each.key
-  storage_account_id    = azurerm_storage_account.crc_cms[each.value].id
-  container_access_type = "blob"
+  name                     = "dctcrccmsbackups${var.env}${var.location}"
+  resource_group_name      = var.resource_group
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+  blob_properties {
+    change_feed_enabled           = true
+    change_feed_retention_in_days = 7
+    last_access_time_enabled      = true
+    versioning_enabled            = true
+    delete_retention_policy {
+      days = 7
+    }
+    container_delete_retention_policy {
+      days = 7
+    }
+  }
+}
+
+#trivy:ignore:avd-azu-0007 storage container public access is enabled as it serves the assets for the website
+resource "azurerm_storage_container" "crc_cms_backups" {
+  count = var.env == "dev" ? 1 : 0
+
+  name                  = "backups"
+  storage_account_id    = azurerm_storage_account.crc_cms_backups[0].id
+  container_access_type = "container"
+}
+
+resource "azurerm_role_assignment" "backups_blob_contributor_pipeline_identity" {
+  count = var.env == "dev" ? 1 : 0
+
+  principal_id         = data.azurerm_client_config.current.object_id
+  role_definition_name = "Storage Blob Data Contributor"
+  scope                = azurerm_storage_account.crc_cms_backups[0].id
+  principal_type       = "ServicePrincipal"
 }
