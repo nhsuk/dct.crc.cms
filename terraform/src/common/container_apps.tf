93ae89717a1b60bd85ce11b7e5c238167646639d
Calvin Merry <104705692+CodeByCalvin@users.noreply.github.com>
2026-04-14 11:00:18 +0100

    Update terraform/src/common/container_apps.tf
    Co-authored-by: Dylan Jones <dylan.jones12@nhs.net>

--- terraform/src/common/container_apps.tf
+++ terraform/src/common/container_apps.tf
@@ -34,4 +34,5 @@ module "aca_wagtail" {
   alerts_action_group_id = module.container_app_env[0].alerts_action_group_id
   dr_origin              = var.dr_deployed ? data.azurerm_container_app.dr[0].ingress[0].fqdn : null
   container_resources    = var.container_resources
+  trim_container_app_resource_names = true
 }
