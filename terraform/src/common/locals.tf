0441dff9d899f86140a9656f31839c3d31e875d4
Dylan Jones <dylan.jones12@nhs.net>
2026-04-15 16:44:15 +0100

    tflint fixes
    * removes unused locals
    * add missing types to variables
    * remove duplicate parameters in logic apps
    * resolve deprecated interpolation

--- terraform/src/common/locals.tf
+++ terraform/src/common/locals.tf
@@ -13,8 +13,6 @@ locals {
   scheduler_logic_app_name         = replace(data.azurerm_resource_group.rg.name, "-rg-", "-scheduler-la-")
   search_reindex_logic_app_name    = replace(data.azurerm_resource_group.rg.name, "-rg-", "-search-reindex-la-")
   key_vault_name                   = replace(data.azurerm_resource_group.rg.name, "-rg-", "-kv2-")
-  log_analytics_workspace_name     = replace(data.azurerm_resource_group.rg.name, "-rg-", "-log-")
-  aks_app_insights_name            = replace(data.azurerm_resource_group.rg.name, "-rg-", "-appi-aks-")
   postgres_flex_name               = replace(data.azurerm_resource_group.rg.name, "-rg-", "-psql-")
   postgres_flex_id                 = "${data.azurerm_resource_group.rg.id}/providers/Microsoft.DBforPostgreSQL/flexibleServers/${local.postgres_flex_name}"
   activeconnections_logic_app_name = replace(data.azurerm_resource_group.rg.name, "-rg-", "-activeconnectionsalert-la-")
@@ -23,7 +21,7 @@ locals {
   backup_vault_resource_group_name = replace(data.azurerm_resource_group.rg.name, "-rg-", "-vault-rg-")
 
   storage_account   = toset(var.storage != null ? [var.storage.account] : [])
-  storage_container = var.storage != null ? { "${var.storage.container}" : var.storage.account } : {}
+  storage_container = var.storage != null ? { var.storage.container : var.storage.account } : {}
 
   secret_names = [
     "alertingWebhook",
